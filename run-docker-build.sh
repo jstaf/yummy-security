f7f36ce876964f0abac98e288ea0ddb535e34243
Jeff Stafford <jeff.stafford@protonmail.com>
2019-06-12 18:14:37 -0400

    fix docker build... again

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -3,6 +3,9 @@
 # cd to wherever this script is in the docker container, build the rpm, then 
 # copy it out
 cd $(dirname "${BASH_SOURCE[0]}")
+mkdir -p ~/rpmbuild/SOURCES
+
 yum-builddep -y yummy-security.spec
 make rpm
+
 cp ~/rpmbuild/RPMS/x86_64/yummy-security*.rpm .
